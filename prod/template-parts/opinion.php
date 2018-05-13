4615febc9a90f898ecf6cd8117205967a3ad78cb
CodingSerwis <maciej.lehrer@codingserwis.pl>
2018-05-13 11:23:52 +0200

    Switching to MAMP - braking changes

--- prod/template-parts/opinion.php
+++
@@ -1,9 +0,0 @@
-		<li>
-			<div class="op_header">
-				<img src="<?php echo types_render_field('zdjecie-opinii', array('url' => 'true'))?>">
-			</div>
-			<blockquote>
-				<?php echo types_render_field('tresc-opinia', array('output' => 'raw'))?>
-				<span><?php echo types_render_field('podpis-opinia', array('output' => 'raw'))?></span>
-			</blockquote>
-		</li>
