9b1487155ac720bc7e2de38bec78fb3bf3941426
CodingSerwis <maciej.lehrer@codingserwis.pl>
2018-05-20 02:24:25 +0200

    ACF version 5

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php 
 /**
+ * Enable ACF 5 early access
+ * Requires at least version ACF 4.4.12 to work
+ */
+define('ACF_EARLY_ACCESS', '5');
+
+/**
  * +++ +++ +++ ENQUEUE STYLES AND SCRIPTS+++ +++ +++
  */
 	function addThemeStylesAndScripts() {
